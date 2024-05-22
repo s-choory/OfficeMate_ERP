99d8c21d20f20b74d1078f4f5a64eed210d623aa
cheol <bora121111@naver.com>
2024-05-22 14:02:21 +0900

    출퇴근 기능 완료 및 페이징처리 오류 수

--- OfficeMate/src/main/java/com/example/demo/service/AttendanceService.java
+++ OfficeMate/src/main/java/com/example/demo/service/AttendanceService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +24,10 @@ public class AttendanceService{
 
 	public int checkOut(AttendanceDTO attendanceDTO) {
 		return attendanceMapper.checkOut(attendanceDTO);
+	}
+
+	public List<AttendanceDTO> getAttendanceByUserId(int userId) {
+		return attendanceMapper.getAttendanceByUserId(userId);
 	}
 
 }
