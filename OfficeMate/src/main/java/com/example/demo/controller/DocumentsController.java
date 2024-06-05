8ada1ad3b4d7596c2992d49409b778f47b699134
cheol <bora121111@naver.com>
2024-06-05 13:35:35 +0900

    관리자 문서조회 오류 수정

--- OfficeMate/src/main/java/com/example/demo/controller/DocumentsController.java
+++ OfficeMate/src/main/java/com/example/demo/controller/DocumentsController.java
@@ -89,7 +89,8 @@ public class DocumentsController {
 		DocumentDTO documentDTO = documentsService.documentOne(documentId);
 
 		String name = SecurityContextHolder.getContext().getAuthentication().getName();
-		if (!name.equals(documentDTO.getUploadUser()) && !name.equals(documentDTO.getShareUser()) && !name.equals("admin")) {
+		if (!name.equals(documentDTO.getUploadUser()) && !name.equals(documentDTO.getShareUser())
+				&& !name.equals("admin")) {
 			return "redirect:/document";
 		}
 
